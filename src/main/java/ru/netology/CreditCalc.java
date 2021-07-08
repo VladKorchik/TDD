be3fca31167a76b8776d417d9a6af34586ff03b5
Vlad Korchagin <vkorchagin007@gmail.com>
2021-07-09 00:33:20 +0300

    Methods added

--- src/main/java/ru/netology/CreditCalc.java
+++ src/main/java/ru/netology/CreditCalc.java
@@ -1,9 +1,22 @@
 package ru.netology;
 
-public class CreditCalc
-{
-    public static void main( String[] args ){
+public class CreditCalc {
 
+    public double countMonthPayment (int randomCreditSum, double ramdomCreditPercent, int randomTimeOfCredit ) {
+        double result = randomCreditSum * (ramdomCreditPercent / 100) / randomTimeOfCredit;
+        return result;
     }
+
+
+    public double countTotalSum (int randomCreditSum, double ramdomCreditPercent) {
+        double result = randomCreditSum * (ramdomCreditPercent / 100 + 1);
+        return result;
+    }
+
+    public double countTotalOverpayments (int randomCreditSum,double ramdomCreditPercent) {
+        double result = (randomCreditSum * (ramdomCreditPercent / 100 + 1)) - randomCreditSum;
+        return result;
+    }
+
 
 }
